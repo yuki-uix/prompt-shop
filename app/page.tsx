f5e1798cd836c343f30434e478b3b1f0c7031afa
yuki.x <yuki.uix@gmail.com>
2026-03-29 14:44:12 +0800

    feat:init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-900 dark:text-gray-100">
+        Prompt Shop
+      </h1>
+    </main>
+  );
+}
